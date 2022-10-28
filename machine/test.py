3491c9a87dce1973775138756ee45a23f5b1aef9
wisesa <trisentosa-wisesa@uiowa.edu>
2022-10-28 17:08:39 -0500

    step motor test

--- machine/test.py
+++ machine/test.py
@@ -1,82 +1,74 @@
-import pyrebase
-'''                   Firebase database initialization and sending temperature data                       '''
-
-# FIREBASE CONFIG AND INITIALIZATION
-config = {
-  "apiKey": "AIzaSyBY8DyNfV_B2NSlKHr4sZRvRT1fBMlOuwQ",
-  "authDomain": "smartdispenser-ac92a.firebaseapp.com",
-  "databaseURL": "https://smartdispenser-ac92a-default-rtdb.firebaseio.com",
-  "projectId": "smartdispenser-ac92a",
-  "storageBucket": "smartdispenser-ac92a.appspot.com",
-  "messagingSenderId": "669426580100",
-  "appId": "1:669426580100:web:b0c2df77a34e687a33e0d1",
-  "measurementId": "G-P7WDJV5GDK"
-}
-
-firebase = pyrebase.initialize_app(config)
-db = firebase.database()
-
-# pump1_status = db.child("pumps").child("pump1").get().val()
-# pump2_status = db.child("pumps").child("pump2").get().val()
-# pump3_status = db.child("pumps").child("pump3").get().val()
-# pump4_status = db.child("pumps").child("pump4").get().val()
-# pump5_status = db.child("pumps").child("pump5").get().val()
-# pump6_status = db.child("pumps").child("pump6").get().val()
-
-# print(pump1_status)
-
-def get_pump1_status():
-    status = db.child("pumps").child("pump1").get().val()
-    return status
-
-def get_pump2_status():
-    status = db.child("pumps").child("pump2").get().val()
-    return status
-
-def get_pump3_status():
-    status = db.child("pumps").child("pump3").get().val()
-    return status
-
-def get_pump4_status():
-    status = db.child("pumps").child("pump4").get().val()
-    return status
-
-def get_pump5_status():
-    status = db.child("pumps").child("pump5").get().val()
-    return status
-
-def get_pump6_status():
-    status = db.child("pumps").child("pump6").get().val()
-    return status
-
-
-print(get_pump1_status())
-print(get_pump2_status())
-print(get_pump3_status())
-print(get_pump4_status())
-print(get_pump5_status())
-print(get_pump6_status())
-
-while True:
-    if(get_pump1_status):
-        GPIO.output(0,GPIO.LOW)
-    else:
-        GPIO.output(0,GPIO.HIGH)
-
-
-# def pump1_toggleON():
-#     if get_pump1_status == "False":
-#         GPIO.output(0,GPIO.LOW)
-#         # time.sleep(2)
-#     else: 
-#         GPIO.output(0,GPIO.HIGH)
-
-
-    
-
-
-
-# if pump1_status == "True":
-#     GPIO.output(0,GPIO.LOW)
-#     time.sleep(2)
-        
+#!/usr/bin/python3
+import RPi.GPIO as GPIO
+import time
+ 
+in1 = 17
+in2 = 18
+in3 = 27
+in4 = 22
+ 
+# careful lowering this, at some point you run into the mechanical limitation of how quick your motor can move
+step_sleep = 0.002
+ 
+step_count = 4096 # 5.625*(1/64) per step, 4096 steps is 360°
+ 
+direction = False # True for clockwise, False for counter-clockwise
+ 
+# defining stepper motor sequence (found in documentation http://www.4tronix.co.uk/arduino/Stepper-Motors.php)
+step_sequence = [[1,0,0,1],
+                 [1,0,0,0],
+                 [1,1,0,0],
+                 [0,1,0,0],
+                 [0,1,1,0],
+                 [0,0,1,0],
+                 [0,0,1,1],
+                 [0,0,0,1]]
+ 
+# setting up
+GPIO.setmode( GPIO.BCM )
+GPIO.setup( in1, GPIO.OUT )
+GPIO.setup( in2, GPIO.OUT )
+GPIO.setup( in3, GPIO.OUT )
+GPIO.setup( in4, GPIO.OUT )
+ 
+# initializing
+GPIO.output( in1, GPIO.LOW )
+GPIO.output( in2, GPIO.LOW )
+GPIO.output( in3, GPIO.LOW )
+GPIO.output( in4, GPIO.LOW )
+ 
+ 
+motor_pins = [in1,in2,in3,in4]
+motor_step_counter = 0 ;
+ 
+ 
+def cleanup():
+    GPIO.output( in1, GPIO.LOW )
+    GPIO.output( in2, GPIO.LOW )
+    GPIO.output( in3, GPIO.LOW )
+    GPIO.output( in4, GPIO.LOW )
+    GPIO.cleanup()
+ 
+ 
+# the meat
+try:
+    i = 0
+    for i in range(step_count):
+        for pin in range(0, len(motor_pins)):
+            GPIO.output( motor_pins[pin], step_sequence[motor_step_counter][pin] )
+        if direction==True:
+            motor_step_counter = (motor_step_counter - 1) % 8
+        elif direction==False:
+            motor_step_counter = (motor_step_counter + 1) % 8
+        else: # defensive programming
+            print( "uh oh... direction should *always* be either True or False" )
+            cleanup()
+            exit( 1 )
+        time.sleep( step_sleep )
+ 
+except KeyboardInterrupt:
+    cleanup()
+    exit( 1 )
+ 
+cleanup()
+exit( 0 )
