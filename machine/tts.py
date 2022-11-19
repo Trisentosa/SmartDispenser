b2e31d6fcfe8313c6979ab5639728e9f7afffd60
wisesa <trisentosa-wisesa@uiowa.edu>
2022-11-19 00:59:53 -0600

    add starter text to speech code

--- machine/tts.py
+++ machine/tts.py
@@ -1,8 +1,18 @@
 from gtts import gTTS
 from playsound import playsound
+import urllib3
 
-def Text_to_speech():
-    Message = entry_field.get()
-    speech = gTTS(text = Message)
-    speech.save('DataFlair.mp3')
-    playsound('DataFlair.mp3')
+urllib3.disable_warnings()
+
+import os
+  
+mytext = 'Hello! welcome to team Kiwi Smart Dispenser machine, Please make your order by clicking the option on the screen or simply say the name of the order to the microphone .' #audio text
+language = 'en' # English Language
+  
+myobj = gTTS(text=mytext, lang=language, slow=False)
+  
+# Saving converted audio
+myobj.save("welcome.mp3")
+  
+# Playing the converted file
+playsound('welcome.mp3')
